bca6db5a474ae343aaf6d3a67a7c4f557caf591b
mario salvatierra <msalv006@fiu.edu>
2019-08-07 16:15:04 -0400

    added env url variable to services in front end

---
+++ server/public/precache-manifest.a3ffa1bd18445724bbecfdced9ac9feb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd7ca5f638d353c4335a5b69384f4c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a82e70f69381d8daf5b7",
+    "url": "/static/css/2.57008913.chunk.css"
+  },
+  {
+    "revision": "d08746c821cb9a151d88",
+    "url": "/static/css/main.fa30e90f.chunk.css"
+  },
+  {
+    "revision": "a82e70f69381d8daf5b7",
+    "url": "/static/js/2.6b1ed1db.chunk.js"
+  },
+  {
+    "revision": "d08746c821cb9a151d88",
+    "url": "/static/js/main.89ea9799.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
